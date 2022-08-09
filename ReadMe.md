8ae5540fa366a1ebd394c08def887616f068ea28
James Odell <jpodell21@gmail.com>
2022-08-09 11:08:55 -0600

    scanf and doubles

--- ReadMe.md
+++ ReadMe.md
@@ -8,3 +8,6 @@
 
 * Run DeBug = Fn + F5
 * Run Build = Ctrl + Shift + B - When trying to run a build for a specific file, you must make sure the task.json file has been updated with the proper source in the args.  
+
+* printf() - Prints the string inside
+* scanf() - Can read a variety of formats. Simple constant strings you can specify with, %s %d %c %f and so on, to read strings, integers, characters or floats. 
---
+++ sampleApp/printf.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+
+int main()
+{
+    printf("Bob: Hello, my name is Bob. Bilbo: Hello Bob, my name is Bilbo. I am a Hobbit, and I am from the Shire. Bob: That's awesome! I hear you have a ring that is super powerful. Bilbo: I have no idea what you are talking about. Bob: Give me precious Bilbo! Bilbo: Noooooooooooooo! Narrator: Bob turns into Gollum. Bob/Gollum: I almost got you! Bilbo: See ya. The end");
+    return 0;
+}
